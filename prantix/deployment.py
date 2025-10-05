a0eccd0cad66b8fbfe099c91fc7a4b31ab51d8f8
Saket Kumar <saket8538@gmail.com>
2025-10-05 23:21:18 +0530

    production.py updated

--- prantix/deployment.py
+++ prantix/deployment.py
@@ -2,7 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']    
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = [f"https://{os.environ['WEBSITE_HOSTNAME']}"]
 DEBUG = False
