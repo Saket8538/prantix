06a6f92e5e83e1936d212505811176859fa7d03b
Saket Kumar <126570621+Saket8538@users.noreply.github.com>
2025-11-11 23:14:03 +0530

    Fix database path to use persisted /home/site/wwwroot directory for SQLite

--- prantix/settings.py
+++ prantix/settings.py
@@ -78,8 +78,7 @@ WSGI_APPLICATION = 'prantix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+ 'NAME': '/home/site/wwwroot/db.sqlite3',    }
 }
 
 
