07cf03569d8456f2b5ae7175733ee2203f66152a
Saket Kumar <126570621+Saket8538@users.noreply.github.com>
2025-11-11 21:55:07 +0530

    Fix ALLOWED_HOSTS to accept all hosts (wildcard)

--- prantix/settings.py
+++ prantix/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-k-_g6qy58vc0i#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
 # Application definition
 
