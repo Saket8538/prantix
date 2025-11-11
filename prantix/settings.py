776d19461eaa67a62222df07e3b7c42566614696
Saket Kumar <126570621+Saket8538@users.noreply.github.com>
2025-11-11 18:40:06 +0530

    Switch database from SQLite to PostgreSQL
    Changed database configuration from SQLite to PostgreSQL.

--- prantix/settings.py
+++ prantix/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'prantix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DBNAME', default=''),
+        'USER': config('DBUSER', default=''),
+        'PASSWORD': config('DBPASS', default=''),
+        'HOST': config('DBHOST', default=''),
+        'PORT': '5432',
     }
 }
 
